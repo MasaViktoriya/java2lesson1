0c24df5632b4abe5f3024781401f7c9c5727bec2
MasaViktoriya <vikoriya.masa@gmail.com>
2022-01-26 17:37:11 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/MarketConstants.java
@@ -0,0 +1,6 @@
+package com.geekbrains;
+
+public class MarketConstants {
+    public static final  String TOMATOES_PRODUCT_NAME = "Помидор";
+    public static final  String CUCUMBERS_PRODUCT_NAME = "Огурец";
+}
